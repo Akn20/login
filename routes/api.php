6dc7d57278a5177f9028cdb01b5b97187c22fcc2
BlxrryFxce17 <akashvmsn@gmail.com>
2026-02-27 23:06:03 +0530

    migration fix

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,16 @@
 <?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReligionController;
-use App\Http\Controllers\JobTypeController;
-use App\Http\Controllers\WorkStatusController;
+
+use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\BloodGroupController;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\DesignationController;
+use App\Http\Controllers\JobTypeController;
 use App\Http\Controllers\ModuleController;
-use App\Http\Controllers\Api\DashboardController;
+use App\Http\Controllers\ReligionController;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\VendorController;
-
+use App\Http\Controllers\WorkStatusController;
+use Illuminate\Support\Facades\Route;
 
 /* Religion */
 
@@ -41,52 +41,56 @@ Route::put('/blood-groups/{id}', [BloodGroupController::class, 'apiUpdate']);
 Route::delete('/blood-groups/{id}', [BloodGroupController::class, 'apiDelete']);
 
 /* Department */
+
 Route::get('/departments', [DepartmentController::class, 'apiIndex']);
 Route::post('/departments', [DepartmentController::class, 'apiStore']);
 Route::put('/departments/{id}', [DepartmentController::class, 'apiUpdate']);
 Route::delete('/departments/{id}', [DepartmentController::class, 'apiDelete']);
 
 /* Designation */
+
 Route::get('/designations', [DesignationController::class, 'apiIndex']);
 Route::post('/designations', [DesignationController::class, 'apiStore']);
 Route::put('/designations/{id}', [DesignationController::class, 'apiUpdate']);
 Route::delete('/designations/{id}', [DesignationController::class, 'apiDelete']);
 
-//For After  deleted records
+/* Religion */
 
-// Religion
 Route::get('religions/deleted', [ReligionController::class, 'apiDeleted']);
 Route::put('religions/{id}/restore', [ReligionController::class, 'apiRestore']);
 Route::delete('religions/{id}/force-delete', [ReligionController::class, 'apiForceDelete']);
 
-// Designation
+/* Designation */
+
 Route::get('designations/deleted', [DesignationController::class, 'apiDeleted']);
 Route::put('designations/{id}/restore', [DesignationController::class, 'apiRestore']);
 Route::delete('designations/{id}/force-delete', [DesignationController::class, 'apiForceDelete']);
 
-// JobType
+/* JobType */
+
 Route::get('job-types/deleted', [JobTypeController::class, 'apiDeleted']);
 Route::put('job-types/{id}/restore', [JobTypeController::class, 'apiRestore']);
 Route::delete('job-types/{id}/force-delete', [JobTypeController::class, 'apiForceDelete']);
 
-// WorkStatus
+/* WorkStatus */
+
 Route::get('work-status/deleted', [WorkStatusController::class, 'apiDeleted']);
 Route::put('work-status/{id}/restore', [WorkStatusController::class, 'apiRestore']);
 Route::delete('work-status/{id}/force-delete', [WorkStatusController::class, 'apiForceDelete']);
 
-// BloodGroup
+/* BloodGroup */
+
 Route::get('blood-groups/deleted', [BloodGroupController::class, 'apiDeleted']);
 Route::put('blood-groups/{id}/restore', [BloodGroupController::class, 'apiRestore']);
 Route::delete('blood-groups/{id}/force-delete', [BloodGroupController::class, 'apiForceDelete']);
 
-// Department
+/* Department */
+
 Route::get('departments/deleted', [DepartmentController::class, 'apiDeleted']);
 Route::put('departments/{id}/restore', [DepartmentController::class, 'apiRestore']);
 Route::delete('departments/{id}/force-delete', [DepartmentController::class, 'apiForceDelete']);
 
-
-
-//Module Management Type Api
+/* Module */
 
 Route::get('/module-types', [ModuleController::class, 'getModuleTypes']);
 
@@ -94,10 +98,10 @@ Route::get('/test-api', function () {
     return 'API working';
 });
 
-
 Route::get('/dashboard', [DashboardController::class, 'index']);
 
-//stock management
+/* Pharmacy API */
+
 Route::prefix('pharmacy')->group(function () {
     Route::get('/stock', [StockController::class, 'apiIndex']);
     Route::get('/stock/low', [StockController::class, 'apiLowStock']);
@@ -108,8 +112,10 @@ Route::prefix('pharmacy')->group(function () {
     Route::delete('stock/{id}', [StockController::class, 'apiDestroy']);
     Route::get('stock-trash', [StockController::class, 'apiTrash']);
     Route::delete('stock-force-delete/{id}', [StockController::class, 'apiForceDelete']);
-//Vendor api
 
+});
+
+/* Vendor API */
 
 Route::prefix('vendors')->group(function () {
 
