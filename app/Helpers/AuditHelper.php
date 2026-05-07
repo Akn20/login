5550fd20f32fe1adc629ad55cc24bd9ab1b57211
Prapthigatty <prapthigatty@gmail.com>
2026-05-07 13:13:40 +0530

    added comment

--- app/Helpers/AuditHelper.php
+++ app/Helpers/AuditHelper.php
@@ -2,6 +2,19 @@
 
 use App\Models\AuditLog;
 
+/*
+|--------------------------------------------------------------------------
+| Global Helper Functions
+|--------------------------------------------------------------------------
+|
+| IMPORTANT:
+| After pulling project updates involving this helper file,
+| run:
+| composer dump-autoload
+| php artisan optimize:clear
+|
+*/
+
 if (!function_exists('logAudit')) {
 
     function logAudit($module, $action, $referenceId = null, $description = null)
