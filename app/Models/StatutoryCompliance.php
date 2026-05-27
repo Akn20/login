53e61ea7802737621e905c7f6b000bbc2cbd22be
Prathikak20 <prathikak8@gmail.com>
2026-05-27 11:38:35 +0530

    Fixed statutory compliance model fillable fields

--- app/Models/StatutoryCompliance.php
+++ app/Models/StatutoryCompliance.php
@@ -16,24 +16,50 @@ class StatutoryCompliance extends Model
 
     public $incrementing = false;
 
-    protected $fillable = [
-        'id',
-        'employee_id',
-        'employee_name',
-        'department',
-        'pf_applicable',
-        'pf_number',
-        'pf_amount',
-        'pf_start_date',
-        'esi_applicable',
-        'esi_number',
-        'esi_amount',
-        'pt_applicable',
-        'pt_amount',
-        'state_applicable',
-        'tds_applicable',
-        'pan_number',
-        'tds_percentage',
+  protected $fillable = [
 
-    ];
+    'id',
+
+    'employee_id',
+    'employee_name',
+    'department',
+
+    // PF
+    'pf_applicable',
+    'pf_number',
+    'pf_amount',
+    'pf_start_date',
+
+    // ESI
+    'esi_applicable',
+    'esi_number',
+    'esi_amount',
+
+    // PT
+    'pt_applicable',
+    'pt_amount',
+    'state_applicable',
+
+    // TDS
+    'tds_applicable',
+    'pan_number',
+    'tds_percentage',
+
+    // Contract
+    'contract_start_date',
+    'contract_end_date',
+    'contract_status',
+
+    // License
+    'license_number',
+    'license_issue_date',
+    'license_expiry_date',
+    'license_upload',
+    'license_status',
+
+    // Additional
+    'remarks',
+    'status',
+
+];
 }
