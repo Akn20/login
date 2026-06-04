968555cd2445ba6d09598faacda0ca2e6a4f1974
Hawaz <hawazmohammed40@gmail.com>
2026-06-04 13:36:04 +0530

    resolved conflicts

--- app/Http/Controllers/doctor/NotificationController.php
+++ app/Http/Controllers/doctor/NotificationController.php
@@ -145,13 +145,13 @@ class NotificationController extends Controller
         );
     }
 
-    
-public function latestNotification()
-{
-    $notification = Notification::latest()->first();
 
-    return response()->json($notification);
-}
+    public function latestNotification()
+    {
+        $notification = Notification::latest()->first();
+
+        return response()->json($notification);
+    }
 
 
 
@@ -208,211 +208,211 @@ public function latestNotification()
 
     public function apiShow($id)
     {
-    $notification = Notification::findOrFail($id);
+        $notification = Notification::findOrFail($id);
 
-    return response()->json([
+        return response()->json([
 
-        'status' => true,
+            'status' => true,
 
-        'data' => $notification
+            'data' => $notification
 
-    ]);
-}
-
-public function apiSearch(Request $request)
-{
-    $query = Notification::query();
-
-    // TYPE FILTER
-    if ($request->type) {
-
-        $query->where(
-            'type',
-            $request->type
-        );
+        ]);
     }
 
-    // STATUS FILTER
-    if ($request->status == 'Unread') {
+    public function apiSearch(Request $request)
+    {
+        $query = Notification::query();
 
-        $query->where(
+        // TYPE FILTER
+        if ($request->type) {
+
+            $query->where(
+                'type',
+                $request->type
+            );
+        }
+
+        // STATUS FILTER
+        if ($request->status == 'Unread') {
+
+            $query->where(
+                'is_read',
+                0
+            );
+        }
+
+        if ($request->status == 'Read') {
+
+            $query->where(
+                'is_read',
+                1
+            );
+        }
+
+        // PRIORITY FILTER
+        if ($request->priority) {
+
+            $query->where(
+                'priority',
+                $request->priority
+            );
+        }
+
+        // SEARCH TITLE
+        if ($request->search) {
+
+            $query->where(
+                'title',
+                'like',
+                '%' . $request->search . '%'
+            );
+        }
+
+        $notifications = $query
+            ->latest()
+            ->get();
+
+        return response()->json([
+
+            'status' => true,
+
+            'data' => $notifications
+
+        ]);
+    }
+
+    public function apiDashboard()
+    {
+        return response()->json([
+
+            'status' => true,
+
+            'data' => [
+
+                'total_notifications' =>
+                    Notification::count(),
+
+                'unread_notifications' =>
+                    Notification::where(
+                        'is_read',
+                        0
+                    )->count(),
+
+                'critical_alerts' =>
+                    Notification::where(
+                        'type',
+                        'Critical'
+                    )->count(),
+
+                'lab_reports' =>
+                    Notification::where(
+                        'type',
+                        'Lab Report'
+                    )->count(),
+
+                'followups' =>
+                    Notification::where(
+                        'type',
+                        'Follow-up'
+                    )->count(),
+
+                'medication_reviews' =>
+                    Notification::where(
+                        'type',
+                        'Medication Review'
+                    )->count(),
+
+                'emergency_alerts' =>
+                    Notification::where(
+                        'type',
+                        'Emergency'
+                    )->count(),
+            ]
+
+        ]);
+    }
+    public function apiMarkAllAsRead()
+    {
+        Notification::where(
             'is_read',
             0
-        );
+        )->update([
+
+                    'is_read' => true
+
+                ]);
+
+        return response()->json([
+
+            'status' => true,
+
+            'message' =>
+                'All notifications marked as read'
+
+        ]);
+    }
+    public function apiLatestNotification()
+    {
+        $notification = Notification::latest()->first();
+
+        return response()->json([
+
+            'status' => true,
+
+            'data' => $notification
+
+        ]);
     }
 
-    if ($request->status == 'Read') {
+    public function apiDelete($id)
+    {
+        $notification = Notification::findOrFail($id);
 
-        $query->where(
+        $notification->delete();
+
+        return response()->json([
+
+            'status' => true,
+
+            'message' =>
+                'Notification deleted successfully'
+
+        ]);
+    }
+
+    public function apiUnread()
+    {
+        $notifications = Notification::where(
+            'is_read',
+            0
+        )
+            ->latest()
+            ->get();
+
+        return response()->json([
+
+            'status' => true,
+
+            'data' => $notifications
+
+        ]);
+    }
+    public function apiRead()
+    {
+        $notifications = Notification::where(
             'is_read',
             1
-        );
+        )
+            ->latest()
+            ->get();
+
+        return response()->json([
+
+            'status' => true,
+
+            'data' => $notifications
+
+        ]);
     }
-
-    // PRIORITY FILTER
-    if ($request->priority) {
-
-        $query->where(
-            'priority',
-            $request->priority
-        );
-    }
-
-    // SEARCH TITLE
-    if ($request->search) {
-
-        $query->where(
-            'title',
-            'like',
-            '%' . $request->search . '%'
-        );
-    }
-
-    $notifications = $query
-        ->latest()
-        ->get();
-
-    return response()->json([
-
-        'status' => true,
-
-        'data' => $notifications
-
-    ]);
-}
-
-public function apiDashboard()
-{
-    return response()->json([
-
-        'status' => true,
-
-        'data' => [
-
-            'total_notifications' =>
-                Notification::count(),
-
-            'unread_notifications' =>
-                Notification::where(
-                    'is_read',
-                    0
-                )->count(),
-
-            'critical_alerts' =>
-                Notification::where(
-                    'type',
-                    'Critical'
-                )->count(),
-
-            'lab_reports' =>
-                Notification::where(
-                    'type',
-                    'Lab Report'
-                )->count(),
-
-            'followups' =>
-                Notification::where(
-                    'type',
-                    'Follow-up'
-                )->count(),
-
-            'medication_reviews' =>
-                Notification::where(
-                    'type',
-                    'Medication Review'
-                )->count(),
-
-            'emergency_alerts' =>
-                Notification::where(
-                    'type',
-                    'Emergency'
-                )->count(),
-        ]
-
-    ]);
-}
-public function apiMarkAllAsRead()
-{
-    Notification::where(
-        'is_read',
-        0
-    )->update([
-
-        'is_read' => true
-
-    ]);
-
-    return response()->json([
-
-        'status' => true,
-
-        'message' =>
-            'All notifications marked as read'
-
-    ]);
-}
-public function apiLatestNotification()
-{
-    $notification = Notification::latest()->first();
-
-    return response()->json([
-
-        'status' => true,
-
-        'data' => $notification
-
-    ]);
-}
-
-public function apiDelete($id)
-{
-    $notification = Notification::findOrFail($id);
-
-    $notification->delete();
-
-    return response()->json([
-
-        'status' => true,
-
-        'message' =>
-            'Notification deleted successfully'
-
-    ]);
-}
-
-public function apiUnread()
-{
-    $notifications = Notification::where(
-        'is_read',
-        0
-    )
-    ->latest()
-    ->get();
-
-    return response()->json([
-
-        'status' => true,
-
-        'data' => $notifications
-
-    ]);
-}
-public function apiRead()
-{
-    $notifications = Notification::where(
-        'is_read',
-        1
-    )
-    ->latest()
-    ->get();
-
-    return response()->json([
-
-        'status' => true,
-
-        'data' => $notifications
-
-    ]);
-}
 
 }
